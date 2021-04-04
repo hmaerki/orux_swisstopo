99e4e85d34d15f63a3639a10cfbcb13cfb5b53f2
Hans Maerki <hans.maerki@zhinst.com>
2021-04-04 11:33:02 +0200

    Before refactoring folder structure

---
+++ orux_swisstopo/create_map_samples.py
@@ -0,0 +1,27 @@
+﻿#!/usr/bin/python
+
+import programm.context
+import programm.constants_switzerland
+import programm.orux_swisstopo
+
+def get_contextA():
+    context = programm.context.Context()
+    # context.skip_optimize_png = True
+    context.only_tiffs = programm.constants_switzerland.tiffs_wetzikon
+    # context.only_tiles_border = 5
+    context.only_tiles_modulo = 10
+    # context.skip_tiff_read = True
+    # context.skip_png_write = True
+    return context
+
+def get_contextB():
+    context = get_contextA()
+    context.skip_optimize_png = True
+    return context
+
+for context in (
+    get_contextA(),
+    get_contextB(),
+):
+    with programm.orux_swisstopo.OruxMap(f"CH_samples{context.parts_png}", context=context) as oruxmap:
+        oruxmap.createLayers(iMasstabMin=10, iMasstabMax=200)
--- orux_swisstopo/schweiz.bat
+++
@@ -1,4 +0,0 @@
-C:\Users\maerki\AppData\Local\Programs\Python\Python37-32\python.exe schweiz.py
-python schweiz.py",
-
-pause
